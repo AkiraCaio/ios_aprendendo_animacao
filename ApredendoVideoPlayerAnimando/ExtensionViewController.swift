d8365484f49e08fc011d336fc5aa1ff988463dca
Caio Pinho <caio_vini2006@hotmail.com>
2020-01-23 15:04:54 -0300

    Corrigido o tamanho do frame pai que nao permitia a interecao com a tableView

--- ApredendoVideoPlayerAnimando/ExtensionViewController.swift
+++ ApredendoVideoPlayerAnimando/ExtensionViewController.swift
@@ -12,8 +12,17 @@ import UIKit
 extension ViewController: VideoPlayerViewDelegate {
     func begin(gestureRecognizer: UIPanGestureRecognizer) {
         if let playerVideo = self.playerVideo {
-
+            
             gestureRecognizer.setTranslation(CGPoint(x: playerVideo.center.x, y: self.positionYVideoPlayer), in: self.view)
+            
+            playerVideo.viewBG!.frame.origin.x =
+                playerVideo.frame.origin.x == 0 ? 0 : ( self.view.bounds.width / 2 )
+            
+            playerVideo.frame.size.width = self.view.bounds.width
+            playerVideo.frame.origin.x = 0
+            
+            playerVideo.frame.size.height = self.view.bounds.height
+            
         }
     }
     
@@ -29,20 +38,17 @@ extension ViewController: VideoPlayerViewDelegate {
                     playerVideo.frame.origin.y = gestureRecognizer.translation(in: self.view).y
                 }
                 
-//                print("\(playerVideo.frame.origin.y)")
-//                print("\(( playerVideo.frame.origin.y ) / self.maxY)")
-//
                 playerVideo.viewBG!.frame.size.width =
-                    ( self.view.bounds.width ) - ( self.view.bounds.width / 2)
+                    ( self.view.bounds.width ) - ( self.view.bounds.width / 2 )
                     * ( ( playerVideo.frame.origin.y ) / self.maxY )
                 
-                playerVideo.viewBG!.frame.origin.x = (self.view.bounds.width / 2)
-                    * ( playerVideo.frame.origin.y ) / self.maxY
+                playerVideo.viewBG!.frame.origin.x = ( self.view.bounds.width / 2 )
+                    * ( ( playerVideo.frame.origin.y ) / self.maxY )
                 
                 playerVideo.viewBG!.frame.size.height =
-                    ( self.view.bounds.height / 3 ) - ( self.view.bounds.height / 6)
+                    ( self.view.bounds.height / 3 ) - ( self.view.bounds.height / 6 )
                     * ( ( playerVideo.frame.origin.y ) / self.maxY )
-    
+                
             }) { (_) in
                 
             }
@@ -51,37 +57,35 @@ extension ViewController: VideoPlayerViewDelegate {
     
     func ended(gestureRecognizer: UIPanGestureRecognizer) {
         if let playerVideo = self.playerVideo {
-            if (gestureRecognizer.translation(in: playerVideo).y >= (self.view.bounds.height / 3)) {
-               
+            if (gestureRecognizer.translation(in: playerVideo).y >= ( self.view.bounds.height / 3 )) {
+                
                 UIView.animate(withDuration: 0.5, animations: {
                     
                     playerVideo.backgroundColor = UIColor.blue.withAlphaComponent(0)
                     
-                    playerVideo.frame.origin.y =  self.view.bounds.height - self.view.bounds.height / 3 - 40
+                    playerVideo.frame.origin.y = self.view.bounds.height - self.view.bounds.height / 3 - 40
                     
-//                    playerVideo.frame.size.width = self.view.bounds.width / 2
-//                    playerVideo.frame.size.height = self.view.bounds.height / 6
+                    playerVideo.frame.size.width = self.view.bounds.width / 2
+                    playerVideo.frame.size.height = self.view.bounds.height / 6
                     
+                    playerVideo.frame.origin.x = self.view.bounds.width / 2
+                    
+                    playerVideo.viewBG!.frame.origin.x = 0
                     
                     playerVideo.viewBG!.frame.size.width = self.view.bounds.width / 2
-                    playerVideo.viewBG!.frame.origin.x = self.view.bounds.width / 2
                     
                     playerVideo.viewBG!.frame.size.height = (self.view.bounds.height / 6)
                     
+                    
                 }) { (_) in
                     self.positionYVideoPlayer = playerVideo.frame.origin.y
-
+                    
                 }
             }else {
                 UIView.animate(withDuration: 0.5, animations: {
                     playerVideo.frame.origin.y = 0
                     
                     playerVideo.backgroundColor = UIColor.blue.withAlphaComponent(1)
-
-//
-//                    playerVideo.frame.size.width = self.view.bounds.width
-//                    playerVideo.frame.size.height = self.view.bounds.height / 3
-                    
                     
                     playerVideo.viewBG!.frame.size.width = playerVideo.bounds.width
                     playerVideo.viewBG!.frame.origin.x = 0
@@ -89,12 +93,10 @@ extension ViewController: VideoPlayerViewDelegate {
                     
                 }) { (_) in
                     self.positionYVideoPlayer = playerVideo.frame.origin.y
-
                 }
             }
-
+            
         }
     }
-    
     
 }
